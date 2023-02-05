123896214938b55a941573e9d3684495b281f170
MariLFO <mari.fer.onorio@gmail.com>
2023-02-04 16:59:46 -0800

    Procedimento 1 completo

--- livros-react/src/LivroLista.js
+++ livros-react/src/LivroLista.js
@@ -12,19 +12,18 @@ const LinhaLivro = props => {
     return (
         <tr>
             <td>
-                <label>Título do livro: {livro.titulo}</label>
-                <button onClick={() => excluir(livro.codigo)}>Excluir</button>
+                <p>{livro.titulo}</p>
+                <button className="btn btn-danger pull-right" onClick={() => excluir(livro.codigo)}>Excluir</button>
             </td>
+            <td>{livro.resumo}</td>
+            <td>{nomeEditora(livro.codEditora)}</td>
             <td>
-                <label>Autores:</label>
                 <ul>
                 {livro.autores.map((autor, index) => (
                     <li key={index}>{autor}</li>
                 ))}
                 </ul>
                 Autores: {livro.titulo}</td>
-            <td>Editora: {nomeEditora(livro.codEditora)}</td>
-            <td></td>
         </tr>
     );
 }
@@ -38,19 +37,35 @@ const LivroLista = () => {
         setCarregado(true);
     }, [carregado]);
 
+    const excluir = (codigo) => {
+        controleLivros.excluir(codigo);
+        setCarregado(false);
+    }
+
     return (
-        <table>
-            <tbody>
-            {livros.map(livro => (
-                <LinhaLivro
-                    key={livro.codigo}
-                    livro={livro}
-                    excluir={controleLivros.excluir}
-                    getNomeEditora={controleEditora.getNomeEditora}
-                />
-            ))}
-            </tbody>
-        </table>
+        <main>
+            <h1>Catálogo de Livros</h1>
+            <table className="table table-hover">
+                <thead className="table-dark">
+                    <tr scope="row">
+                        <th>Título</th>
+                        <th>Resumo</th>
+                        <th>Editora</th>
+                        <th>Autores</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {livros.map(livro => (
+                    <LinhaLivro
+                        key={livro.codigo}
+                        livro={livro}
+                        excluir={excluir}
+                        getNomeEditora={controleEditora.getNomeEditora}
+                    />
+                ))}
+                </tbody>
+            </table>
+        </main>
     )
 }
 
