5574dcbfe494fd176b6d6d5b47f665cbe60cc7e0
MariLFO <mari.fer.onorio@gmail.com>
2023-02-04 18:29:40 -0800

    Implementar o tratamento de requisições

--- livros-next/pages/api/livros/index.ts
+++ livros-next/pages/api/livros/index.ts
@@ -4,4 +4,8 @@ import {NextApiRequest, NextApiResponse} from "next";
 export const controleEditora = new ControleEditora();
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        const editoras = controleEditora.getEditoras();
+        return res.status(200).json(editoras);
+    }
 }
