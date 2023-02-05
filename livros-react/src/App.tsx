4af55d8ec41953eba57d827ce2609ec589860a5f
MariLFO <mari.fer.onorio@gmail.com>
2023-02-04 17:14:29 -0800

    Procedimento 2 - navegação

--- livros-react/src/App.tsx
+++ livros-react/src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import './LivroLista.js';
 
 import LivroLista from "./LivroLista";
+import LivroDados from "./LivroDados";
 
 function App() {
   return (
-    <LivroLista/>
+      <React.StrictMode>
+        <BrowserRouter>
+          <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+              <Link to="/" className="navbar-brand">Catálogo</Link>
+              <Link to="dados" className="navbar-brand">Dados</Link>
+            </div>
+          </nav>
+          <Routes>
+            <Route path="/" element={<LivroLista />} />
+            <Route path="dados" element={<LivroDados />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
   );
 }
 
---
+++ livros-react/src/LivroDados.js
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from "react";
+
+const LivroDados = () => {
+    return (
+        <main>Olá mundo</main>
+    )
+}
+export default LivroDados;
